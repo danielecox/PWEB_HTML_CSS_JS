b2babed4d5c5fe33e5439e1ff1d643e69bee44e1
Daniele Marighetti Cox <daniele.marighetti@gmail.com>
2021-10-23 11:16:22 -0300

    PWEB - JS - HTML - CSS - BOOTTRAP - JQUERY

---
+++ pw07/pw07_correcao/pw07.js
@@ -0,0 +1,31 @@
+﻿function somar(){
+	var a = parseFloat(n1.value);
+	var b = parseFloat(n2.value);
+	var r = a+b;	
+	resultado.value = r;
+}
+
+function subtrair(){
+	var a = parseFloat(n1.value);
+	var b = parseFloat(n2.value);
+	var r = a-b;	
+	resultado.value = r;
+}
+
+function multiplicar(){
+	var a = parseFloat(n1.value);
+	var b = parseFloat(n2.value);
+	var r = a*b;	
+	resultado.value = r;
+}
+
+function dividir(){
+	var a = parseFloat(n1.value);
+	var b = parseFloat(n2.value);
+	if(b==0){
+		resultado.value = "divisão por zero !";
+	} else {
+		var r = a/b;	
+		resultado.value = r;
+	}
+}
---
+++ pw07/pw07_correcao/versao2/pw07.js
@@ -0,0 +1,31 @@
+﻿function somar(obj1, obj2, objRes){
+	var a = parseFloat(obj1.value);
+	var b = parseFloat(obj2.value);
+	var r = a+b;	
+	objRes.value = r;
+}
+
+function subtrair(obj1, obj2, objRes){
+	var a = parseFloat(obj1.value);
+	var b = parseFloat(obj2.value);
+	var r = a-b;	
+	objRes.value = r;
+}
+
+function multiplicar(obj1, obj2, objRes){
+	var a = parseFloat(obj1.value);
+	var b = parseFloat(obj2.value);
+	var r = a*b;	
+	objRes.value = r;
+}
+
+function dividir(obj1, obj2, objRes){
+	var a = parseFloat(obj1.value);
+	var b = parseFloat(obj2.value);
+	if(b==0){
+		objRes.value = "divisão por zero !";
+	} else {
+		var r = a/b;	
+		objRes.value = r;
+	}
+}
